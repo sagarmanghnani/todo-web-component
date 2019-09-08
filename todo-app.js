165f8dee85aa739c05fa232b5569aeeb2151f531
sagar <sagarmanghnani@gmail.com>
2019-09-08 21:58:04 +0530

    now todo can be added using input

--- todo-app.js
+++ todo-app.js
@@ -11,6 +11,9 @@ class TodoApp extends HTMLElement{
         this._shadowRoot = this.attachShadow({'mode': 'open'});
         this._shadowRoot.appendChild(template.content.cloneNode(true));
         this.$todoList = this._shadowRoot.querySelector('ul');
+        this.$input = this._shadowRoot.querySelector('input');
+        this.$submitButton = this._shadowRoot.querySelector('button');
+        this.$submitButton.addEventListener('click', this._addTodo.bind(this));
     }
 
     _renderTodoList(){
@@ -20,6 +23,16 @@ class TodoApp extends HTMLElement{
             $todoItem.innerHTML = todo.text;
             this.$todoList.appendChild($todoItem);
         });
+    }
+
+    _addTodo(){
+        if(this.$input.value.length > 0){
+            this._todos.push({
+                text:this.$input.value, checked: false
+            });
+            this._renderTodoList();
+            this.$input.value = '';
+        }
     }
 
     set todos(value){
